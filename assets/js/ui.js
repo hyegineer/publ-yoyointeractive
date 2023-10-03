e391bd6dd121c1068c890e000a978ffad746c374
jeonghyejin <hyegineer@gmail.com>
2023-10-04 03:49:50 +0900

    요요 납품

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -15,15 +15,29 @@ $(".opt-list-grp .opt-list").on("click", function () {
 // ========================== 헤더 ) hover 시 서브메뉴 노출
 $(".header .nav-wrap .nav-list").on("mouseover", function () {
   $(".header").addClass("active");
+  $("#hd-search").slideUp();
 })
 
 $(".header").on("mouseleave", function () {
   $(".header").removeClass("active");
 })
 
+// ========================== 헤더 ) 검색버튼 클릭시
+$("#search-open-btn").on("click", function () {
+  $(".header").removeClass("active"); 
+  $("#hd-search").slideDown();
+})
+
+$("#hd-search").on("mouseleave", function () {
+  $("#hd-search").slideUp();
+})
+
 // ========================== 헤더 ) 확장형 메뉴
 $("#nav-open-btn").on("click", function () {
   $("#nav-page").addClass("active");
+  if ($("#main-cursor")) {
+    $("#main-cursor").removeClass("show");
+  }
 })
 
 $("#nav-close-btn").on("click", function () {
@@ -43,3 +57,35 @@ $(".nav-list").on("mouseleave", function () {
   $(".nav-link").removeClass("active");
   $(".snb-list").removeClass("active");
 })
+// ========================== 팝업
+function showPopup(id) {
+  $('.popup-container').addClass('active');
+  $('#' + id).addClass('active');
+  $('#' + id).children('.popup-backdrop').show();
+  $('#' + id).children('.popup').show();
+}
+
+function closeAllPopup() {
+  $('.popup-container').removeClass('active');
+  $('.popup-wrapper').removeClass('active');
+}
+
+function clickBackdrop(id) {
+  $('#' + id).removeClass('active');
+}
+// ========================== 헤더 ) hover 시 서브메뉴 노출
+$(function(){
+  var $header = $('.header'); //헤더를 변수에 넣기
+  var $page = $('.scroll-change'); //색상이 변할 부분
+  var $window = $(window);
+  var pageOffsetTop = $page.offset().top;//색상 변할 부분의 top값 구하기
+  
+  $window.on('resize', function(){ //반응형을 대비하여 리사이즈시 top값을 다시 계산
+    pageOffsetTop = $page.offset().top;
+  });
+  
+  $window.on('scroll', function(){ //스크롤시
+    var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
+    $header.toggleClass('bg-white', scrolled); //클래스 토글
+  });
+});
